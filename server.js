f1955e1fd1816560a1aa63b727fc8df32b323886
Triston Tetley <tetleylax@aol.com>
2019-11-25 17:29:53 -0500

    added all the folder and files I will need to create this app and wrote out the code for the server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
+require("./routes/product-api-routes")(app);
+
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("APP listening on port: " + PORT);
+    });
+});
