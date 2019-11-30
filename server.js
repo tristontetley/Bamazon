0f1c27f4dbe419a4b232b06177af23013313bf83
Triston Tetley <tetleylax@aol.com>
2019-11-30 15:59:51 -0500

    fixed routing issue in my api-routes file and added basic content to my index.html file

--- server.js
+++ server.js
@@ -10,12 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
-
-db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("APP listening on port: " + PORT);
-    });
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("APP listening on port: " + PORT);
+  });
 });
